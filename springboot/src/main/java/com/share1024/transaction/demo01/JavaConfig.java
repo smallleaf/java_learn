56727bbf18945ed06092e525e3285f1e4212fa02
yesheng <yesheng@huya.com>
2021-05-28 21:20:38 +0800

    AOp

--- springboot/src/main/java/com/share1024/transaction/demo01/JavaConfig.java
+++ springboot/src/main/java/com/share1024/transaction/demo01/JavaConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -16,6 +17,7 @@ import javax.sql.DataSource;
  */
 @Configuration
 @ComponentScan("com.share1024.transaction.demo01")
+@EnableTransactionManagement
 public class JavaConfig {
 
     @Bean
@@ -23,8 +25,8 @@ public class JavaConfig {
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123456");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
         return dataSource;
     }
 
