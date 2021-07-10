e3afa15413975d1dd8848b0a20a0f5a223ecdb00
yesheng <yesheng@huya.com>
2021-07-10 19:58:42 +0800

    canal

--- sentinel/src/main/java/com/share1024/demo/HelloWorld.java
+++ sentinel/src/main/java/com/share1024/demo/HelloWorld.java
@@ -9,6 +9,7 @@ import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * \* @Author: yesheng
@@ -26,9 +27,12 @@ public class HelloWorld {
             try (Entry entry = SphU.entry("HelloWorld")) {
                 // 被保护的逻辑
                 System.out.println("hello world");
+                TimeUnit.MILLISECONDS.sleep(20);
             } catch (BlockException ex) {
                 // 处理被流控的逻辑
                 System.out.println("blocked!");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
